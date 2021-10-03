2e759259b9b64f8ae8392dac552b1fad5673b5eb
Mohd Nisab Alam <nisabmohd@gmail.com>
2021-10-03 21:01:30 +0530

    commit

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-const xhr=new XMLHttpRequest();
-let mbody=document.getElementById('mbody')
-xhr.open('GET','https://meme-api.herokuapp.com/gimme',true)
+const xhr = new XMLHttpRequest();
+let mbody = document.getElementById('mbody')
+xhr.open('GET', 'https://meme-api.herokuapp.com/gimme', true)
 xhr.getResponseHeader('content-type', 'application/json');
-xhr.onload=()=>{
-    let json=JSON.parse(xhr.responseText);
-    let meme=json.url
-    let author=json.author
-
-    mbody.innerHTML=`<div class="card" style="width: 35rem;">
+xhr.onprogress=()=>{
+}
+xhr.onload = () => {
+  let json = JSON.parse(xhr.responseText);
+  let meme = json.url
+  let author = json.author
+  mbody.innerHTML = `<div class="card" style="width: 35rem;">
     <img src="${meme}" class="card-img-top" alt="...">
     <div class="card-body">
       <p class="card-text">Author: ${author}</p>
@@ -16,6 +17,6 @@ xhr.onload=()=>{
     `
 }
 xhr.send();
-let btn=document.getElementById('btn').addEventListener('click',()=>{
-    location.reload();
+let btn = document.getElementById('btn').addEventListener('click', () => {
+  location.reload();
 })
